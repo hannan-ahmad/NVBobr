9ad20cc074ed3ebdda78b8fbadedd5f661f626cc
Abdul Hannan Ahmed <kyoyagami666kof@gmail.com>
2025-06-04 18:56:12 +0500

    Update colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,27 @@
 return {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+        { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+        { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+
+        { "gbprod/nord.nvim" },
+
+        {
+			"folke/tokyonight.nvim",
+			lazy = false,
+			priority = 1000,
+			opts = {},
+        },
+
+        {
+			"olimorris/onedarkpro.nvim",
+			priority = 1000,
+        },
+
+        { "sainnhe/gruvbox-material" },
+
+        { "loctvl842/monokai-pro.nvim" },
+
+        { "Shatur/neovim-ayu" },
+
+        { "rose-pine/neovim" },
 }
