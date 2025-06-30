5d8325d460b6b0ebc5a2f4e6b86c50e0e88078e2
Abdul Hannan Ahmed <kyoyagami666kof@gmail.com>
2025-06-30 11:03:11 +0500

    Update colorscheme.lua
    Added night owl and dracula themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,35 +1,39 @@
 return {
-        { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
-        { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
 
-        { "gbprod/nord.nvim" },
+	{ "gbprod/nord.nvim" },
 
-        {
-                "folke/tokyonight.nvim",
-                lazy = false,
-                priority = 1000,
-                opts = {},
-        },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
 
-        {
-                "olimorris/onedarkpro.nvim",
-                priority = 1000,
-        },
+	{
+		"olimorris/onedarkpro.nvim",
+		priority = 1000,
+	},
 
-        { "sainnhe/gruvbox-material" },
+	{ "sainnhe/gruvbox-material" },
 
-        { "loctvl842/monokai-pro.nvim" },
+	{ "loctvl842/monokai-pro.nvim" },
 
-        { "Shatur/neovim-ayu" },
+	{ "Shatur/neovim-ayu" },
 
-        { "rose-pine/neovim" },
+	{ "rose-pine/neovim" },
 
-        { "rebelot/kanagawa.nvim" },
+	{ "rebelot/kanagawa.nvim" },
 
-        { "askfiy/visual_studio_code" },
+	{ "askfiy/visual_studio_code" },
 
-        { "sainnhe/everforest" },
+	{ "sainnhe/everforest" },
 
-        { "projekt0n/github-nvim-theme" },
+	{ "projekt0n/github-nvim-theme" },
+
+	{ "oxfist/night-owl.nvim" },
+
+	{ "Mofiqul/dracula.nvim" },
 }
