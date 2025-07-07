3a2f43798a98d35a15da56f1e58a8b1557b7a1c2
Abdul Hannan Ahmed <kyoyagami666kof@gmail.com>
2025-07-07 21:54:56 +0500

    Update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,15 @@ local function auto_run()
 	if file_type == "c" then
 		vim.cmd.split()
 		vim.cmd.terminal("gcc " .. file_name .. "&& ./a.out")
+		vim.cmd.call("nvim_feedkeys('A', 'n', v:true)")
+	elseif file_type == "python" then
+		vim.cmd.split()
+		vim.cmd.terminal("python3.13 " .. file_name)
+		vim.cmd.call("nvim_feedkeys('A', 'n', v:true)")
+	elseif file_type == "sh" then
+		vim.cmd.split()
+		vim.cmd.terminal("bash " .. file_name)
+		vim.cmd.call("nvim_feedkeys('A', 'n', v:true)")
 	end
 end
 
@@ -14,13 +23,16 @@ local function open_terminal()
 	vim.cmd.call("nvim_feedkeys('A', 'n', v:true)")
 end
 
+local function format()
+	require("conform").format()
+end
+
 local map = vim.keymap.set
 
 map("n", "<leader>rn", auto_run)
 map("n", "<leader>n", ":tabnew<CR>")
 map("n", "<tab>", ":tabnext<CR>")
 map("n", "<S-tab>", ":tabprevious<CR>")
-map("n", "<leader>ft", vim.lsp.buf.format)
 map("n", "<C-q>", ":q<CR>")
 map("n", "<C-j>", open_terminal)
 
@@ -31,6 +43,5 @@ map("n", "<leader>ff", ":Telescope find_files<CR>")
 map("n", "<leader>th", ":Telescope colorscheme<CR>")
 map("n", "<leader>fw", ":Telescope live_grep<CR>")
 map("n", "<leader>ld", ":Telescope diagnostics<CR>")
-map("n", "<leader>ff", ":Telescope find_files<CR>")
-map("n", "<leader>ff", ":Telescope find_files<CR>")
-map("n", "<leader>ff", ":Telescope find_files<CR>")
+
+map("n", "<leader>ft", format)
